2f2bde165e89d9726e8004e2972b8e202a59ccf1
fairickoo <61010656@kmitl.ac.th>
2020-05-31 14:37:06 +0700

    properties & widget

--- waystory/lib/main.dart
+++ waystory/lib/main.dart
@@ -5,9 +5,11 @@ void main () => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage(),);
+    return MaterialApp(
+      home: HomePage(),
+      title: 'App',
+      );
   }
-
 }
 
 
--- waystory/lib/page/home.dart
+++ waystory/lib/page/home.dart
@@ -4,7 +4,15 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // appBar: AppBar(
+      //   title:Text('begin'),
+      // ),
+      body: 
+        Container(
+          child: Text('Hello world'),
+        ),
+    );
   }
 
 }
